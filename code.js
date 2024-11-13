c0298fc0b7aafbc4b5b9a8d82550b90a958ecabe
GravyTheAwesome <sapengluan@gmail.com>
2024-11-13 19:24:26 +0800

    Move code creating grid to separate function

--- code.js
+++ code.js
@@ -1,21 +1,25 @@
 const body = document.querySelector('body');
 
-const grid = document.createElement('div');
-grid.classList.add('grid');
-body.appendChild(grid);
+function createGrid(size) {
+    const grid = document.createElement('div');
+    grid.classList.add('grid');
+    body.appendChild(grid);
 
-for (let i = 0; i < 16; i++) {
-    const grid_row = document.createElement('div');
-    grid_row.classList.add('grid-row');
-    grid.appendChild(grid_row);
-    for (let j = 0; j < 16; j++) {
-        const grid_square = document.createElement('div');
-        grid_square.classList.add('grid-square');
-        grid_row.appendChild(grid_square);
+    for (let i = 0; i < size; i++) {
+        const grid_row = document.createElement('div');
+        grid_row.classList.add('grid-row');
+        grid.appendChild(grid_row);
+        for (let j = 0; j < size; j++) {
+            const grid_square = document.createElement('div');
+            grid_square.classList.add('grid-square');
+            grid_row.appendChild(grid_square);
+        }
     }
+
+    const grid_squares = document.querySelectorAll('.grid-square');
+    grid_squares.forEach((grid_square) => {
+        grid_square.addEventListener('mouseover', () => {grid_square.style.backgroundColor = 'black'});
+    })
 }
 
-const grid_squares = document.querySelectorAll('.grid-square');
-grid_squares.forEach((grid_square) => {
-    grid_square.addEventListener('mouseover', () => {grid_square.style.backgroundColor = 'black'});
-})
+createGrid(16);
